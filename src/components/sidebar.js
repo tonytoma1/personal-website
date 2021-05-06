844e206af942b14890c008a53b7715bb093e7ac9
Tony <tony.toma2@outlook.com>
2021-05-06 12:48:28 -0600

    aligned bullet points

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,20 +6,23 @@ export default function Sidebar() {
             <div className="sidebar-top-content">
                 <div className="top-container">
                     <img src="/profile-pic-holder.png" />
-                    <h2>Tony Toma</h2>
-                    <h4>Hello, I am Tony and I am a software developer from Canada</h4>
+                    <h2 >Tony Toma</h2>
+                    <h4 className="header">Hello, I am Tony and I am a software developer from Canada</h4>
                 </div>
             </div>
 
             <div className="sidebar-middle-content">
-                <ul>
-                    <li>Linkedin</li>
-                    <li>Resume</li>
-                    <li>Github</li>
-                    <li>Email</li>
-                    <li>Hire Me</li>
-                    
-                </ul>
+                <div className="middle-container">
+                    <h3>Contact Links</h3>
+                    <ul>
+                        <li>Linkedin</li>
+                        <li>Resume</li>
+                        <li>Github</li>
+                        <li>Email</li>
+                        <li>Hire Me</li>
+                        
+                    </ul>
+                </div>
             </div>
 
             <div className="sidebar-bottom-content">
